43e9eff7bfe950ba81bf21ab742a5b3476e1ef94
RomanMavrin <chessmanager@yandex.ru>
2026-02-21 13:09:14 +0300

    Изменение

--- Project4.py
+++ Project4.py
@@ -1,4 +1,4 @@
-'''import datetime
+import datetime
 from collections import Counter
 from typing import List
 
@@ -17,7 +17,7 @@ def most_common_months(dates: List[str], n) -> List[int]:
 
 
 dates=["2023-01-01T23:59:59", "2023-01-01T23:59:59", "2023-02-01T23:59:59"]
-print(most_common_months(dates, 2))'''
+print(most_common_months(dates, 2))
 
 from collections import deque
 from typing import List
